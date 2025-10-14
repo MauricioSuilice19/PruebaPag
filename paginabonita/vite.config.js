32374c6fb1fd6b66c05baabcc02591f621e450c9
MauricioSuilice19 <lucasuilice@hotmail.com.ar>
2025-10-14 11:03:00 -0300

    refactor: simplificar configuración de Vite eliminando opciones de construcción innecesarias

--- paginabonita/vite.config.js
+++ paginabonita/vite.config.js
@@ -1,31 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: false,
-    cssCodeSplit: true,
-    chunkSizeWarningLimit: 500,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom'],
-        },
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
-        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
-      }
-    },
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    }
-  }
-    }),
-  ],
-})
+});
