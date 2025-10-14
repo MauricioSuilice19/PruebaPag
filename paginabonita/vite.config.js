df1271d1e6e4cec5884c3666021cd6912772a155
MauricioSuilice19 <lucasuilice@hotmail.com.ar>
2025-10-14 10:59:27 -0300

    feat: agregar configuración de Netlify y optimización de la construcción con Terser

--- paginabonita/vite.config.js
+++ paginabonita/vite.config.js
@@ -3,11 +3,29 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
+  plugins: [react()],
+  build: {
+    sourcemap: false,
+    cssCodeSplit: true,
+    chunkSizeWarningLimit: 500,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
+      }
+    },
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
+  }
     }),
   ],
 })
