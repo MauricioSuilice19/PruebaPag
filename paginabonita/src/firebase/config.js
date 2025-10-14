98c20d55bd1f8795a4e5eb0c92f440da760d49a0
MauricioSuilice19 <lucasuilice@hotmail.com.ar>
2025-10-14 12:29:51 -0300

    feat: integrate Firebase into the project
    - Added Firebase dependency to package.json
    - Removed obsolete _redirects file
    - Created .firebase/hosting.ZGlzdA.cache for hosting configuration
    - Added .firebaserc for Firebase project settings
    - Created firebase.json for hosting setup
    - Implemented Firebase configuration in src/firebase/config.js

---
+++ paginabonita/src/firebase/config.js
@@ -0,0 +1,18 @@
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCrZ0kWRgKnteuH6DLMXYdzmCmaNOg4fK8",
+  authDomain: "paginabonita-9fb3f.firebaseapp.com",
+  projectId: "paginabonita-9fb3f",
+  storageBucket: "paginabonita-9fb3f.firebasestorage.app",
+  messagingSenderId: "36877418303",
+  appId: "1:36877418303:web:80b1bde41c01b9cbcd9d8a",
+  measurementId: "G-69E5YX00VP",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export default app;
